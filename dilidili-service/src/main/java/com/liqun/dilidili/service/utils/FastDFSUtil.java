aa8fbb38f0f0e58c3d0b9a9d7a03f171c3d310dd
LiQunOrigin <2915132212@qq.com>
2025-02-18 10:32:16 +0800

    fastDFS断点续传

--- dilidili-service/src/main/java/com/liqun/dilidili/service/utils/FastDFSUtil.java
+++ dilidili-service/src/main/java/com/liqun/dilidili/service/utils/FastDFSUtil.java
@@ -5,11 +5,15 @@ import com.github.tobato.fastdfs.domain.fdfs.StorePath;
 import com.github.tobato.fastdfs.service.AppendFileStorageClient;
 import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.liqun.dilidili.domain.exception.ConditionException;
+import io.netty.util.internal.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class FastDFSUtil {
@@ -19,6 +23,9 @@ public class FastDFSUtil {
 
     @Autowired
     private AppendFileStorageClient appendFileStorageClient;
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
 
     private static final String DEFAULT_GROUP = "group1";
 
@@ -49,6 +56,59 @@ public class FastDFSUtil {
 
     public void modifyAppenderFile(MultipartFile file, String filePath, long offset) throws IOException {
         appendFileStorageClient.modifyFile(DEFAULT_GROUP, filePath, file.getInputStream(), file.getSize(), offset);
+    }
+
+    private static final String PATH_KEY = "path-key:";
+    private static final String UPLOADED_SIZE_KEY = "upload-size-key:";
+    private static final String UPLOADED_NO_KEY = "upload-no-key:";
+
+    public String uploadFileBySlices(MultipartFile file, String fileMd5, Integer sliceNo, Integer totalSliceNo) throws IOException {
+        /**
+         *
+         */
+        if (file == null || sliceNo == null || totalSliceNo == null) {
+            throw new ConditionException("参数异常！");
+        }
+        String pathKey = PATH_KEY + fileMd5;// 文件路径的key
+        String uploadedSizeKey = UPLOADED_SIZE_KEY + fileMd5;// 上传大小key
+        String uploadedNoKey = UPLOADED_NO_KEY + fileMd5;// 上传块的key
+        String uploadedSizeStr = redisTemplate.opsForValue().get(uploadedSizeKey);
+        Long uploadedSize = 0L;
+        if (StringUtil.isNullOrEmpty(uploadedSizeStr)) {
+            uploadedSize = Long.valueOf(uploadedSizeStr);
+        }// 上传大小
+        String fileType = this.getFileType(file);// 文件类型
+        if (sliceNo == 1) {// 如果是第一块，那么就初始化
+            String path = this.uploadAppenderFile(file);
+            if (StringUtil.isNullOrEmpty(path)) {
+                throw new ConditionException("上传失败！");
+            }
+            redisTemplate.opsForValue().set(pathKey, path);// 设置文件路径
+            uploadedSize += file.getSize();
+            redisTemplate.opsForValue().set(uploadedSizeKey, String.valueOf(uploadedSize));
+            redisTemplate.opsForValue().set(uploadedNoKey, "1");
+        } else {
+            String filePath = redisTemplate.opsForValue().get(pathKey);
+            if (StringUtil.isNullOrEmpty(filePath)) {
+                throw new ConditionException("文件不存在！");
+            }
+            this.modifyAppenderFile(file, filePath, uploadedSize);
+            redisTemplate.opsForValue().increment(uploadedNoKey);
+        }
+        //修改历史上传分片文件大小
+
+        uploadedSize += file.getSize();
+        redisTemplate.opsForValue().set(uploadedSizeKey, String.valueOf(uploadedSize));
+        //如果上传分片文件数量等于总分片文件数量，则清空redis中的数据
+        String uploadedNoStr = redisTemplate.opsForValue().get(uploadedNoKey);
+        Integer uploadedNo = Integer.valueOf(uploadedNoStr);
+        String resultPath = "";
+        if(uploadedNo.equals(totalSliceNo)) {
+            resultPath = redisTemplate.opsForValue().get(pathKey);
+            List<String> keyList = Arrays.asList(pathKey, uploadedSizeKey, uploadedNoKey);
+            redisTemplate.delete(keyList);
+        }
+        return resultPath;
     }
 
 
